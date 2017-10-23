030b2d5992ad758ed36fd755643bbdcefff8cf8e
miyaque <miyaquey@gmail.com>
2017-10-23 10:23:01 +0200

    Vylepseno dle rady kouca z PyLadies

--- domaci_projekt3_uloha13.py
+++ domaci_projekt3_uloha13.py
@@ -9,6 +9,7 @@ n = 50
 
 def house():
     for i in range(2):
+        t.pendown()
         t.forward(n)
         t.right(90)
         t.forward(4/3*n)
@@ -18,16 +19,15 @@ def house():
     t.right(120)
     t.forward(n)
     t.left(60)
+    t.penup()
 
 
 
 for i in range (2):
     for j in range(2):
-        t.penup()
         j *= -1
         i *= -1
         t.goto(j*(100), i*(-200))
-        t.pendown()
         house()
 
 
